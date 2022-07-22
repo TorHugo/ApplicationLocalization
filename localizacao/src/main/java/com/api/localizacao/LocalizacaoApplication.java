87891d8c3221b1ff284b985ad3e9808d03c878a9
Victor Hugo de Arruda Jesus <88560162+TorHugo@users.noreply.github.com>
2022-07-22 19:25:45 -0300

    Adicionando Repository da entidade Cidade.

--- localizacao/src/main/java/com/api/localizacao/LocalizacaoApplication.java
+++ localizacao/src/main/java/com/api/localizacao/LocalizacaoApplication.java
@@ -1,15 +1,34 @@
 package com.api.localizacao;
 
+import com.api.localizacao.domain.entity.Cidade;
+import com.api.localizacao.domain.repository.CidadeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.transaction.annotation.Transactional;
 
 @SpringBootApplication
 public class LocalizacaoApplication implements CommandLineRunner {
 
+	@Autowired
+	private CidadeRepository cidadeRepository;
+
 	@Override
 	public void run(String... args) throws Exception {
+		saveCidade();
+		listCidades();
+	}
 
+	@Transactional
+	void saveCidade(){
+		var cidade = new Cidade(1L, "São Paulo", 12396372L);
+		cidadeRepository.save(cidade);
+	}
+
+	@Transactional
+	void listCidades(){
+		cidadeRepository.findAll().forEach(System.out::println);
 	}
 
 	public static void main(String[] args) {
---
+++ localizacao/src/main/java/com/api/localizacao/domain/repository/CidadeRepository.java
@@ -0,0 +1,7 @@
+package com.api.localizacao.domain.repository;
+
+import com.api.localizacao.domain.entity.Cidade;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CidadeRepository extends JpaRepository<Cidade, Long> {
+}
